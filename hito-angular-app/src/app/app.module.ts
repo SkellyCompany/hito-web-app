2bf560a9e57b70d5209f90c25786a2aba2c19567
kidagine <36992613+kidagine@users.noreply.github.com>
2020-04-28 03:51:29 +0200

    Added the initial component.

--- hito-angular-app/src/app/app.module.ts
+++ hito-angular-app/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
+import { InitialComponent } from './main/initial/initial.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InitialComponent
   ],
   imports: [
     BrowserModule,
