f2e8a6176b73be9b6a1f8b1f0163067bd41e4b35
kidagine <36992613+kidagine@users.noreply.github.com>
2020-04-28 03:31:24 +0200

    Connected angular app to firebase.

--- hito-angular-app/src/environments/environment.ts
+++ hito-angular-app/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBHwJoYTGIMo8nHX0wDceaNLK75Eh7J5u8",
+    authDomain: "hito-33569.firebaseapp.com",
+    databaseURL: "https://hito-33569.firebaseio.com",
+    projectId: "hito-33569",
+    storageBucket: "hito-33569.appspot.com",
+    messagingSenderId: "488691710883",
+    appId: "1:488691710883:web:4ae2938b14b977fd9b7816",
+    measurementId: "G-2LMP39YJBG"
+  }
 };
 
 /*
