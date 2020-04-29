bc8d128565826136ade6cb6ecc10e0aea6627ce6
schemabuoi <charyszczakg@gmail.com>
2020-04-29 13:37:14 +0200

    Update routing, initial component and login component

--- hito-angular-app/src/app/app-routing.module.ts
+++ hito-angular-app/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { InitialComponent } from './main/initial/initial.component';
+import {LoginComponent} from "./main/initial/login/login.component";
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: InitialComponent,  },
+  { path: '', component: InitialComponent,
+    children: [
+      { path: 'login', component: LoginComponent}
+  ]},
 ];
 
 @NgModule({
