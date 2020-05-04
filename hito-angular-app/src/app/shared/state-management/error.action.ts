5fa9b480bfdc51156cc75b08c09eddde93c8c1ca
kidagine <36992613+kidagine@users.noreply.github.com>
2020-05-04 02:33:45 +0200

    Initial commit of functions.

--- hito-angular-app/src/app/shared/state-management/error.action.ts
+++
@@ -1,7 +0,0 @@
-import { ErrorMessage } from './../models/error-message';
-
-export class ErrorOccurred {
-  static readonly type = '[ErrorMessage] Error Occurred';
-
-  constructor(public error: ErrorMessage) {}
-}
